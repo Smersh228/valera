0a2bbd6282eae87b79b30df4054ea72e8eb5a052
Smersh228 <89087089+Smersh228@users.noreply.github.com>
2023-02-06 14:46:48 +0300

    1
    1

---
+++ javascript.js
@@ -0,0 +1,42 @@
+const valera = document.createElement("h2")
+valera.innerHTML = "VALERA LOH"
+valera.className = "s"
+let e = 0
+function Click() {
+ const div = document.querySelector(".sueta")
+  
+  
+e++
+//document.querySelector(".sueta").remove()
+document.querySelector(".pp").innerHTML=`${e}`
+console.log(e)
+
+if (e === 11) {
+
+ 
+
+document.body.after(valera)
+} 
+else {
+valera.remove()
+
+
+}
+
+if (e > 12) {
+
+    document.body.remove()
+    
+}
+
+
+
+}
+
+
+
+
+//d(starm1)
+//div_sueta.addEventListener("click", () => {starm1++})
+
+
